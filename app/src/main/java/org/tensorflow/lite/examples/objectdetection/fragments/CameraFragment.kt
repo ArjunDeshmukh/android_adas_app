da72faf8fa131bc40b25f24f9a9c354373ec1d62
Deshmukh <arjun.deshmukh@aptiv.com>
2023-06-15 00:21:57 +0530

    First commit: Event Triggered photo capture

---
+++ app/src/main/java/org/tensorflow/lite/examples/objectdetection/fragments/CameraFragment.kt
@@ -0,0 +1,421 @@
+/*
+ * Copyright 2022 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *             http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.tensorflow.lite.examples.objectdetection.fragments
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.ContextWrapper
+import android.content.res.Configuration
+import android.graphics.Bitmap
+import android.os.Build
+import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.Surface
+import android.view.Surface.ROTATION_0
+import android.view.Surface.ROTATION_270
+import android.view.Surface.ROTATION_90
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.Toast
+import androidx.camera.core.AspectRatio
+import androidx.camera.core.Camera
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageAnalysis.OUTPUT_IMAGE_FORMAT_RGBA_8888
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.core.VideoCapture
+import androidx.camera.core.impl.utils.ContextUtil.getBaseContext
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.video.FallbackStrategy
+import androidx.camera.video.Quality
+import androidx.camera.video.QualitySelector
+import androidx.camera.video.Recorder
+import androidx.camera.video.Recording
+import androidx.camera.view.PreviewView
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.rotationMatrix
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import java.util.LinkedList
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+import org.tensorflow.lite.examples.objectdetection.ObjectDetectorHelper
+import org.tensorflow.lite.examples.objectdetection.R
+import org.tensorflow.lite.examples.objectdetection.databinding.FragmentCameraBinding
+import org.tensorflow.lite.task.vision.detector.Detection
+import java.io.File
+import java.nio.ByteBuffer
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+class CameraFragment : Fragment(), ObjectDetectorHelper.DetectorListener {
+
+    private val TAG = "ObjectDetection"
+
+    private var _fragmentCameraBinding: FragmentCameraBinding? = null
+
+    private val fragmentCameraBinding
+        get() = _fragmentCameraBinding!!
+
+    private lateinit var objectDetectorHelper: ObjectDetectorHelper
+    private lateinit var bitmapBuffer: Bitmap
+    private var preview: Preview? = null
+    private var imageAnalyzer: ImageAnalysis? = null
+    private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+
+    /** Blocking camera operations are performed using this executor */
+    private lateinit var cameraExecutor: ExecutorService
+
+    private var imageCapture: ImageCapture? = null
+
+    private var videoCapture: androidx.camera.video.VideoCapture<Recorder>? = null
+    private var recording: Recording? = null
+
+    override fun onResume() {
+        super.onResume()
+        // Make sure that all permissions are still present, since the
+        // user could have removed them while the app was in paused state.
+        if (!PermissionsFragment.hasPermissions(requireContext())) {
+            Navigation.findNavController(requireActivity(), R.id.fragment_container)
+                .navigate(CameraFragmentDirections.actionCameraToPermissions())
+        }
+    }
+
+    override fun onDestroyView() {
+        _fragmentCameraBinding = null
+        super.onDestroyView()
+
+        // Shut down our background executor
+        cameraExecutor.shutdown()
+    }
+
+    override fun onCreateView(
+      inflater: LayoutInflater,
+      container: ViewGroup?,
+      savedInstanceState: Bundle?
+    ): View {
+        _fragmentCameraBinding = FragmentCameraBinding.inflate(inflater, container, false)
+
+        return fragmentCameraBinding.root
+    }
+
+    @SuppressLint("MissingPermission")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        objectDetectorHelper = ObjectDetectorHelper(
+            context = requireContext(),
+            objectDetectorListener = this)
+
+        // Initialize our background executor
+        cameraExecutor = Executors.newSingleThreadExecutor()
+
+        // Wait for the views to be properly laid out
+        fragmentCameraBinding.viewFinder.post {
+            // Set up the camera and its use cases
+            setUpCamera()
+        }
+
+        // Attach listeners to UI control widgets
+        initBottomSheetControls()
+    }
+
+    private fun initBottomSheetControls() {
+        // When clicked, lower detection score threshold floor
+        fragmentCameraBinding.bottomSheetLayout.thresholdMinus.setOnClickListener {
+            if (objectDetectorHelper.threshold >= 0.1) {
+                objectDetectorHelper.threshold -= 0.1f
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, raise detection score threshold floor
+        fragmentCameraBinding.bottomSheetLayout.thresholdPlus.setOnClickListener {
+            if (objectDetectorHelper.threshold <= 0.8) {
+                objectDetectorHelper.threshold += 0.1f
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, reduce the number of objects that can be detected at a time
+        fragmentCameraBinding.bottomSheetLayout.maxResultsMinus.setOnClickListener {
+            if (objectDetectorHelper.maxResults > 1) {
+                objectDetectorHelper.maxResults--
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, increase the number of objects that can be detected at a time
+        fragmentCameraBinding.bottomSheetLayout.maxResultsPlus.setOnClickListener {
+            if (objectDetectorHelper.maxResults < 5) {
+                objectDetectorHelper.maxResults++
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, decrease the number of threads used for detection
+        fragmentCameraBinding.bottomSheetLayout.threadsMinus.setOnClickListener {
+            if (objectDetectorHelper.numThreads > 1) {
+                objectDetectorHelper.numThreads--
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, increase the number of threads used for detection
+        fragmentCameraBinding.bottomSheetLayout.threadsPlus.setOnClickListener {
+            if (objectDetectorHelper.numThreads < 4) {
+                objectDetectorHelper.numThreads++
+                updateControlsUi()
+            }
+        }
+
+        // When clicked, change the underlying hardware used for inference. Current options are CPU
+        // GPU, and NNAPI
+        fragmentCameraBinding.bottomSheetLayout.spinnerDelegate.setSelection(0, false)
+        fragmentCameraBinding.bottomSheetLayout.spinnerDelegate.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    objectDetectorHelper.currentDelegate = p2
+                    updateControlsUi()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    /* no op */
+                }
+            }
+
+        // When clicked, change the underlying model used for object detection
+        fragmentCameraBinding.bottomSheetLayout.spinnerModel.setSelection(0, false)
+        fragmentCameraBinding.bottomSheetLayout.spinnerModel.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                    objectDetectorHelper.currentModel = p2
+                    updateControlsUi()
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {
+                    /* no op */
+                }
+            }
+    }
+
+    // Update the values displayed in the bottom sheet. Reset detector.
+    private fun updateControlsUi() {
+        fragmentCameraBinding.bottomSheetLayout.maxResultsValue.text =
+            objectDetectorHelper.maxResults.toString()
+        fragmentCameraBinding.bottomSheetLayout.thresholdValue.text =
+            String.format("%.2f", objectDetectorHelper.threshold)
+        fragmentCameraBinding.bottomSheetLayout.threadsValue.text =
+            objectDetectorHelper.numThreads.toString()
+
+        // Needs to be cleared instead of reinitialized because the GPU
+        // delegate needs to be initialized on the thread using it when applicable
+        objectDetectorHelper.clearObjectDetector()
+        fragmentCameraBinding.overlay.clear()
+    }
+
+    // Initialize CameraX, and prepare to bind the camera use cases
+    private fun setUpCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+        cameraProviderFuture.addListener(
+            {
+                // CameraProvider
+                cameraProvider = cameraProviderFuture.get()
+
+                // Build and bind the camera use cases
+                bindCameraUseCases()
+            },
+            ContextCompat.getMainExecutor(requireContext())
+        )
+    }
+
+    // Declare and bind preview, capture and analysis use cases
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun bindCameraUseCases() {
+
+        // CameraProvider
+        val cameraProvider =
+            cameraProvider ?: throw IllegalStateException("Camera initialization failed.")
+
+        // CameraSelector - makes assumption that we're only using the back camera
+        val cameraSelector =
+            CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build()
+
+        // Preview. Only using the 4:3 ratio because this is the closest to our models
+        preview =
+            Preview.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .build()
+                .also {
+                        it.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+                      }
+
+
+        // ImageAnalysis. Using RGBA 8888 to match how our models work
+        imageAnalyzer =
+            ImageAnalysis.Builder()
+                .setTargetAspectRatio(AspectRatio.RATIO_4_3)
+                .setTargetRotation(fragmentCameraBinding.viewFinder.display.rotation)
+                .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                .setOutputImageFormat(OUTPUT_IMAGE_FORMAT_RGBA_8888)
+                .build()
+                // The analyzer can then be assigned to the instance
+                .also {
+                    it.setAnalyzer(cameraExecutor) { image ->
+                        if (!::bitmapBuffer.isInitialized) {
+                            // The image rotation and RGB image buffer are initialized only once
+                            // the analyzer has started running
+                            //image.imageInfo.sensorToBufferTransformMatrix = Matrix
+                            //var rotMat = rotationMatrix(90F, 0.0F, 0.0F)
+                            bitmapBuffer = Bitmap.createBitmap(
+                              image.width,
+                              image.height,
+                              Bitmap.Config.ARGB_8888
+                            )
+                        }
+
+                        detectObjects(image)
+                    }
+                }
+
+        imageCapture = ImageCapture.Builder().build()
+
+        val recorder = Recorder.Builder()
+            .setQualitySelector(QualitySelector.from(Quality.HIGHEST,
+                FallbackStrategy.higherQualityOrLowerThan(Quality.SD)))
+            .build()
+        videoCapture = androidx.camera.video.VideoCapture.withOutput(recorder)
+
+        // Must unbind the use-cases before rebinding them
+        cameraProvider.unbindAll()
+
+        try {
+            // A variable number of use-cases can be passed here -
+            // camera provides access to CameraControl & CameraInfo
+            camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageAnalyzer, imageCapture)
+
+            // Attach the viewfinder's surface provider to preview use case
+            //preview?.setSurfaceProvider(fragmentCameraBinding.viewFinder.surfaceProvider)
+        } catch (exc: Exception) {
+            Log.e(TAG, "Use case binding failed", exc)
+        }
+    }
+
+    private fun detectObjects(image: ImageProxy) {
+        // Copy out RGB bits to the shared bitmap buffer
+        image.use { bitmapBuffer.copyPixelsFromBuffer(image.planes[0].buffer) }
+
+        val imageRotation = image.imageInfo.rotationDegrees
+        // Pass Bitmap and rotation to the object detector helper for processing and detection
+        objectDetectorHelper.detect(bitmapBuffer, imageRotation)
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        imageAnalyzer?.targetRotation = fragmentCameraBinding.viewFinder.display.rotation
+    }
+
+    // Update UI after objects have been detected. Extracts original image height/width
+    // to scale and place bounding boxes properly through OverlayView
+    override fun onResults(
+      results: MutableList<Detection>?,
+      inferenceTime: Long,
+      imageHeight: Int,
+      imageWidth: Int
+    ) {
+        activity?.runOnUiThread {
+            fragmentCameraBinding.bottomSheetLayout.inferenceTimeVal.text =
+                            String.format("%d ms", inferenceTime)
+
+            // Pass necessary information to OverlayView for drawing on the canvas
+            fragmentCameraBinding.overlay.setResults(
+                results ?: LinkedList<Detection>(),
+                imageHeight,
+                imageWidth
+            )
+
+            // Force a redraw
+            fragmentCameraBinding.overlay.invalidate()
+        }
+    }
+
+    override fun onError(error: String) {
+        activity?.runOnUiThread {
+            Toast.makeText(requireContext(), error, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun takePhoto() {
+        // Get a stable reference of the modifiable image capture use case
+        val imageCapture = imageCapture ?: return
+
+        // Create time stamped name and MediaStore entry.
+        val name = SimpleDateFormat(FILENAME_FORMAT, Locale.US)
+            .format(System.currentTimeMillis())
+        val contentValues = ContentValues().apply {
+            put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            if(Build.VERSION.SDK_INT > Build.VERSION_CODES.P) {
+                put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/CameraX-Image")
+            }
+        }
+
+        // Create output options object which contains file + metadata
+        val outputOptions = activity?.applicationContext?.let {
+            ImageCapture.OutputFileOptions
+                .Builder(
+                    it.contentResolver,
+                    MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
+                    contentValues)
+                .build()
+        }
+
+        // Set up image capture listener, which is triggered after photo has
+        // been taken
+        if (outputOptions != null) {
+            imageCapture.takePicture(
+                outputOptions,
+                ContextCompat.getMainExecutor(requireContext()),
+                object : ImageCapture.OnImageSavedCallback {
+                    override fun onError(exc: ImageCaptureException) {
+                        Log.e(TAG, "Photo capture failed: ${exc.message}", exc)
+                    }
+
+                    @SuppressLint("RestrictedApi")
+                    override fun
+                            onImageSaved(output: ImageCapture.OutputFileResults){
+                        val msg = "Photo capture succeeded: ${output.savedUri}"
+                        Toast.makeText(getBaseContext(ContextWrapper(requireContext())), msg, Toast.LENGTH_SHORT).show()
+                        Log.d(TAG, msg)
+                    }
+                }
+            )
+        }
+    }
+
+    companion object {
+        private const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
+    }
+}
